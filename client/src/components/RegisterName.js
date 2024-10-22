aac1649a8d7e623c673af06330045d43e5f06a23
Christian G <christianlee.gonzalez@calbaptist.edu>
2024-10-21 20:51:33 -0700

    Implemented a red border for an invalid name when registering.

--- client/src/components/RegisterName.js
+++ client/src/components/RegisterName.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './CSS/RegisterName.css';
 import { useNavigate } from "react-router-dom";
 
 const RegisterName = () => {
+    const [firstName, setFirstName] = useState("");     // Value of input field.
+    const [error, setError] = useState(false);             // Tracks whether input is empty and should show an error
+    const navigate = useNavigate();     
 
-    const navigate = useNavigate();
 
-    const handleNextButton = () => {
-        console.log("Next page");
+    const handleNextButton = (e) => {
+        e.preventDefault(); 
+
+        // If the name is empty set red border. 
+        if(firstName.trim() == "") {
+            setError(true);
+        } else {
+            setError(false);
+            console.log("next page.")       // Handle navigation to next page
+        }
     }
 
-    const handleBackButton = () => {
-        navigate('/welcome');
+    const handleBackButton = (e) => {
+        e.preventDefault();
+        navigate('/welcome');       // Goes back to previous page
     }
     
     return (
@@ -22,8 +33,11 @@ const RegisterName = () => {
                 <input
                 type="text"
                 placeholder="First Name"
-                required
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className={error ? 'input-error' : ''}
                 />
+                {error && <p className="error-message">Please enter a first name.</p>}
                 <div className="button-containers">
                     <button className="back-button" onClick={handleBackButton}>BACK</button>
                     <button onClick={handleNextButton} className="next-button">NEXT</button>
