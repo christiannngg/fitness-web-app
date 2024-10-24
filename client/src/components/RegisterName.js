cb100061562f2add15da3eee7fa739192eafd5c9
Christian Gonzalez <christianleegonzalez9@gmail.com>
2024-10-24 09:51:29 -0700

    Red border around the invalid input when registering

--- client/src/components/RegisterName.js
+++ client/src/components/RegisterName.js
@@ -14,6 +14,7 @@ const RegisterName = () => {
         // If the name is empty set red border. 
         if(firstName.trim() === "") {
             setError(true);
+            
         } else {
             setError(false);
             console.log("next page.")       // Handle navigation to next page
@@ -30,12 +31,12 @@ const RegisterName = () => {
             <form className="register-form">
                 <h1 className="register-header">Enter your first name</h1>
                 <p className="register-desc">We are happy you're tracking with us!</p>
-                <input id="first-name-input"
+                <input 
                 type="text"
                 placeholder="First Name"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
-                className={error ? 'input-error' : ''}
+                className={error ? 'first-name-error' : 'first-name-input'}
                 />
                 {error && <p className="error-message">Please enter a first name.</p>}
                 <div className="button-containers">
